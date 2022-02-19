96c255dfea16256a9330c797ec69d0f0b8314c92
https://github.com/MaxKondratiev <78102464+MaxKondratiev@users.noreply.github.com>
2022-02-19 12:44:19 -0600

    created Unit test Bundle

---
+++ fromMvcToMvvmTests/fromMvcToMvvmTests.swift
@@ -0,0 +1,33 @@
+//
+//  fromMvcToMvvmTests.swift
+//  fromMvcToMvvmTests
+//
+//  Created by максим  кондратьев  on 19.02.2022.
+//
+
+import XCTest
+@testable import fromMvcToMvvm
+
+class fromMvcToMvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
